d7bd8a71124f9476dacdfe257045270f77f86056
awais-ansari <awaisansari1502@gmail.com>
2024-12-20 18:07:44 +0000

    created the header section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import {Inter, Calistoga} from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -6,14 +7,35 @@ export const metadata: Metadata = {
   description: "My portfolio website built with Next.js",
 };
 
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  style: "normal",
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+  
+});
+
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-calistoga",
+  style: "normal",
+ weight: ["400"],
+  display: "swap",
+ 
+});
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: Readonly<{   
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" >
+      <body className={`${inter.variable}  ${calistoga.variable} bg-gray-900 text-white antialiased font-sans`}>
+        {children}
+        </body>
     </html>
   );
 }
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,28 @@
-export const Header = () => {
-  return <div>Header Section</div>;
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-center items-center relative top-3">
+      <nav className="flex gap-1 p-0.5 border border-white/15 bg-white/10 rounded-full backdrop-blur">
+        <Link href={"#"} className="nav-item">
+          Home
+        </Link>
+        <Link href={"#"} className="nav-item">
+          Projects
+        </Link>
+        <Link href={"#"} className="nav-item">
+          About
+        </Link>
+        <Link
+          href={"#"}
+          className="nav-item bg-white text-gray-900 hover:text-gray-900 hover:bg-white/70"
+        >
+          Contact
+        </Link>
+      </nav>
+    </div>
+  );
 };
+
+export default Header;
